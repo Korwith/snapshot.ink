5bbba3fb73f909e5d0c7b7ec7875f14cbc101d12
Korwith <caseaway01@gmail.com>
2024-06-13 16:25:00 -0400

    1.0

---
+++ handle.js
@@ -0,0 +1,79 @@
+const content_holder = document.querySelector('.content_holder');
+const placeholder_seperator = document.querySelector('#placeholder');
+const header_button = document.querySelectorAll('header .button_select');
+const image_data = {
+    '06/09/24': [1175, 1186, 1188, 1193, 1198, 1199, 1203, 1209],
+    '07/21/22': [8086, 8090, 8093, 8120, 8153],
+    '11/28/21': [1043, 1074, 1213, 1307, 1311, 1314],
+}
+const date_title = {
+    '11/28/21': 'Downtown Frederick w/ Paris',
+    '07/21/22': 'Hershey Park w/ Riley',
+    '06/09/24': 'Railroad Tracks & Monocacy River',
+}
+
+let active_button;
+
+function assignHeaderFunction() {
+    function buttonClicked(event) {
+        if (active_button == event.target) { return false };
+        if (active_button) {
+            active_button.classList.remove('active');
+        }
+        event.target.classList.add('active');
+        active_button = event.target;
+    }
+
+    for (var i = 0; i < header_button.length; i++) {
+        let this_button = header_button[i];
+        this_button.onclick = buttonClicked;
+    }
+}
+
+function addSeperator(date) {
+    let clone = placeholder_seperator.cloneNode(true);
+    let clone_text = clone.querySelector('.seperator_text');
+    clone.removeAttribute('id');
+    clone_text.innerHTML = `${date}: ${date_title[date]}`
+    content_holder.appendChild(clone);
+}
+
+function loadAlbum(date) {
+    let this_data = image_data[date];
+    addSeperator(date);
+    
+    for (var i = 0; i < this_data.length; i++) {
+        let image_id = this_data[i];
+        let preview_url = `url(media/preview/IMG_${image_id}.jpg)`;
+        let full_url = `url(media/full/IMG_${image_id}.jpg)`;
+
+        let content_frame = document.createElement('div');
+        content_frame.classList.add('content_frame');
+        content_holder.appendChild(content_frame);
+        
+        content_frame.style.setProperty('--preview_url', preview_url);
+        content_frame.addEventListener('mouseenter', imageHovered);
+        content_frame.addEventListener('mouseleave', imageUnhovered);
+        content_frame.addEventListener('mousedown', imageClick);
+    }
+}
+
+function imageHovered() {
+    content_holder.classList.add('image_hovered')
+}
+
+function imageUnhovered() {
+    content_holder.classList.remove('image_hovered');
+}
+
+function imageClick() {
+
+}
+
+assignHeaderFunction();
+
+let image_keys = Object.keys(image_data);
+for (var i = 0; i < image_keys.length; i++) {
+    let key = image_keys[i];
+    loadAlbum(key);
+}

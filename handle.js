99389c77f749722810fd34bd1fcda634eaa18668
Korwith <caseaway01@gmail.com>
2024-06-14 12:26:32 -0400

    DIDNT WORK X2

--- handle.js
+++ handle.js
@@ -94,8 +94,8 @@ function makeAlbum(date) {
             let new_x = adjusted_x - photo_select_left;
             let offset = new_x / photo_select_width;
             let found_index = Math.round(circle_count * offset);
-            shiftPhoto(clone, found_index);
             alert(found_index);
+            //shiftPhoto(clone, found_index);
         }
 
         clone_photo_select.addEventListener('touchstart', function(event) {
